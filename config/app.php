8910ddfb18ac05fd135d633c648872ad5a844563
Rajib Bin Alam <41984925+rajibbinalam@users.noreply.github.com>
2025-01-11 10:47:35 +0600

    Update app.php

--- config/app.php
+++ config/app.php
@@ -96,8 +96,8 @@ return [
     'fallback_locale' => 'en',
 
 
-    'base_url' => 'http://api.greenweb.com.bd/api.php',
-    'smkey' => '67901853011722516781ffe1dc3062682a0ae3bcbd1c63395b9f',
+    'base_url' => '',
+    'smkey' => '',
     'count' => 0,
 
     /*
